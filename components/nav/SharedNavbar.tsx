82a8884c761f02065bc699730f81b3c17f06d960
matvey <35634442+matrv@users.noreply.github.com>
2026-03-02 14:28:02 +0100

    remove @headlessui/react (replace with popover api)

--- components/nav/SharedNavbar.tsx
+++ components/nav/SharedNavbar.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { Transition } from "@headlessui/react";
 import { DownloadIcon, MenuIcon } from "lucide-react";
 import { useTranslations } from "next-intl";
 import { Link, usePathname, useRouter } from "@/i18n/routing";
 import LanguageSelector from "@/components/LanguageSelector";
 import DownloadBtn from "@/components/download-btn";
+import { cn } from "@/lib/utils";
 
 interface SharedNavbarProps {
   variant?: "home" | "page";
@@ -41,10 +41,9 @@ const SharedNavbar = ({ variant = "page", onScrollTo }: SharedNavbarProps) => {
 
   return (
     <nav
-      className={`fixed ${isTop && !navbarOpen ? "top-4" : "top-0 lg:top-4"} z-30 w-full text-white transition duration-300 ease-in-out`}
-    >
+      className={cn('fixed select-none z-30 w-full text-white transition-[top] ease-out top-0 lg:top-4 has-[:popover-open]:top-0', isTop && "top-4")} >
       <div
-        className={`container mx-auto flex w-full flex-col flex-wrap items-center p-6 md:flex-row lg:w-5/6 lg:p-2 ${isTop && !navbarOpen ? "bg-transparent" : "bg-gray-300/10 px-4 drop-shadow-xl backdrop-blur-2xl lg:rounded-3xl"} ${navbarOpen && isTop ? "pt-10" : ""}`}
+        className={cn(`container mx-auto transition-[background-color,padding,backdrop-filter] flex w-full flex-col flex-wrap items-center p-6 md:flex-row lg:w-5/6 lg:p-2 ease-out has-[:popover-open]:bg-gray-300/10 has-[:popover-open]:px-4 has-[:popover-open]:backdrop-blur-2xl has-[:popover-open]:lg:rounded-3xl lg:rounded-3xl`, isTop ? "bg-transparent" : "bg-gray-300/10 px-4 drop-shadow-xl backdrop-blur-2xl")}
       >
         {/* Desktop navbar */}
         <div className="mb-4 hidden w-full flex-row items-center justify-between gap-4 font-medium text-white md:mb-0 lg:flex">
@@ -196,7 +195,7 @@ const SharedNavbar = ({ variant = "page", onScrollTo }: SharedNavbarProps) => {
             <button
               className="block cursor-pointer rounded border border-solid border-transparent bg-transparent px-3 py-1 text-xl leading-none text-white outline-none focus:outline-none lg:hidden"
               type="button"
-              onClick={() => setNavbarOpen(!navbarOpen)}
+              popoverTarget="mobile-menu"
             >
               <MenuIcon width={30} height={30} />
             </button>
@@ -204,16 +203,10 @@ const SharedNavbar = ({ variant = "page", onScrollTo }: SharedNavbarProps) => {
         </div>
 
         {/* Mobile menu */}
-        <Transition
-          show={navbarOpen}
-          enter="transition ease-out duration-100"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-          className="w-full"
-          as="div"
+        <div
+          popover="auto"
+          id="mobile-menu"
+          className="mx-0 opacity-0 w-full mt-[5.25rem] transition-discrete ease-out open:scale-100 transition-opacity open:opacity-100 starting:open:opacity-0 bg-gray-300/10 backdrop-blur-2xl text-white"
         >
           <div className="mt-5 flex-grow items-center rounded-2xl lg:flex">
             <div className="flex w-full flex-col items-center justify-center gap-8 p-10 text-lg">
@@ -254,7 +247,7 @@ const SharedNavbar = ({ variant = "page", onScrollTo }: SharedNavbarProps) => {
               </Link>
             </div>
           </div>
-        </Transition>
+        </div>
       </div>
     </nav>
   );
